98a6337c1f83b82b3a313b6212580d55c3d78d4b
Raymond.Raymond <Raymond.Raymond@carsales.com.au>
2015-10-12 13:01:11 +1100

    Reusable Components

--- script.jsx
+++ script.jsx
@@ -1,30 +1,63 @@
 var Button = React.createClass(
-{
-  	getInitialState: function()
+{  	
+	localHandleClick: function()
 	{
-		return {
-			counter: 0
-		};
-	},
-	handleClick: function()
-	{
-		this.setState(
-		{ 
-			counter: this.state.counter + 1
-		});
+		this.props.localHandleClick(this.props.increment);
 	},
 	render: function()
 	{
 		return(
-			<button onClick={this.handleClick}>
-				{this.state.counter}
+			<button onClick={this.localHandleClick}>
+				+{this.props.increment}
 			</button>
 		);
 	}
 });
 
+var Result = React.createClass(
+{
+  	render: function()
+	{
+		return(
+			<div>
+				{this.props.localCounter}
+			</div>
+		);
+	}
+});
+
+var Main = React.createClass(
+{
+	getInitialState: function()
+	{
+		return {
+			counter: 0
+		};
+	},
+	handleClick: function(increment)
+	{
+		this.setState(
+		{ 
+			counter: this.state.counter + increment
+		});
+	},
+  	render: function()
+	{
+		return(
+			<div>
+				<Button localHandleClick={this.handleClick} increment={1} />
+				<Button localHandleClick={this.handleClick} increment={5} />
+				<Button localHandleClick={this.handleClick} increment={10} />
+				<Button localHandleClick={this.handleClick} increment={100} />
+				<Result localCounter={this.state.counter} />
+			</div>
+		);
+	}
+});
+
+
 ReactDOM.render
 (
-  <Button />,
+  <Main />,
   document.getElementById("root")
 );
