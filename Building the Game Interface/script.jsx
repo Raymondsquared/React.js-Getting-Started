788a41e9c1cd206c5f93c9399e0a9c764f777dd3
Raymond.Raymond <Raymond.Raymond@carsales.com.au>
2015-10-12 18:55:49 +1100

    The Main Game Component

---
+++ Building the Game Interface/script.jsx
@@ -0,0 +1,17 @@
+var Game = React.createClass(
+{
+  	render: function()
+	{
+		return(
+			<div id="game">
+				<h2> Play Nine </h2>
+			</div>
+		);
+	}
+});
+
+ReactDOM.render
+(
+  	<Game />,
+  	document.getElementById("container")
+);
